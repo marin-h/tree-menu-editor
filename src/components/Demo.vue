e9db78e9c3491848ff5b71344f30f5efe3ebb13c
gonzalomvillanueva <gonzalomvillanueva@users.noreply.github.com>
2021-10-28 09:34:48 -0300

    Style, buttons, toasts added.

---
+++ src/components/Demo.vue
@@ -0,0 +1,63 @@
+<!-- the demo root element -->
+      
+  
+<script setup>
+// demo data
+var treeData = {
+  name: "My Tree",
+  children: [
+    { name: "hello" },
+    { name: "wat" },
+    {
+      name: "child folder",
+      children: [
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }]
+        },
+        { name: "hello" },
+        { name: "wat" },
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }]
+        }
+      ]
+    }
+  ]
+};
+
+// define the tree-item component
+        
+  
+// boot up the demo
+var demo = new Vue({
+  el: "#demo",
+  data: {
+    treeData: treeData
+  },
+  methods: {
+    makeFolder: function(item) {
+      Vue.set(item, "children", []);
+      this.addItem(item);
+    },
+    addItem: function(item) {
+      item.children.push({
+        name: "new stuff"
+      });
+    }
+  }
+});
+</script>
+
+<template>
+  <ul id="demo">
+    <tree-item
+      class="item"
+      :item="treeData"
+      @make-folder="makeFolder"
+      @add-item="addItem"
+    ></tree-item>
+  </ul>
+</template>
+  
+</template>
